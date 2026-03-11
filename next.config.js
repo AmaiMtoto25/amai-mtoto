335b79fb837997c91e0314270eb7c58cb6af9a47
AmaiMtoto25 <120182737+AmaiMtoto25@users.noreply.github.com>
2026-03-11 23:02:11 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// module.exports = nextConfig;
-
-import * as dotenv from "dotenv";
-dotenv.config();
-
-export default nextConfig;
+module.exports = nextConfig;
