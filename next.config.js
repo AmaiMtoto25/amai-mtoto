0834123c357a35719a4054b8a6f7fd642573710a
AmaiMtoto25 <120182737+AmaiMtoto25@users.noreply.github.com>
2026-03-13 13:06:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+// updated
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
